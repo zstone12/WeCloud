5a12dd2398ba609c92298844e5c9f2dda8da2b2a
budebao <531648642@qq.com>
2020-05-26 11:38:38 +0800

    s'

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,11 @@ from api import views
 urlpatterns = [
     path('test/',views.Test.as_view()),
     path('checkUsername',views.CheckUsername.as_view()),
-    path('checkEmail',views.CheckEmail.as_view())
+    path('checkEmail',views.CheckEmail.as_view()),
+    path('reg',views.Reg.as_view()),
+    path('login',views.Login.as_view()),
+    path('isLog',views.IsLog.as_view()),
+    path('cheakPassword',views.CheckPassword.as_view()),
+    path('outLogin',views.OutLogin.as_view())
+
 ]
--- api/views.py
+++ api/views.py
@@ -6,8 +6,13 @@ from django.http import request
 from rest_framework.response import Response
 from api.serializers import  *
 from api.models import  *
+import hashlib
 
-
+def md5(str):
+    m1 = hashlib.md5()
+    m1.update(str.encode(encoding='UTF-8'))
+    token = m1.hexdigest()
+    return token
 class BaseResponse(object):
     def __init__(self):
         self.code = 200
@@ -26,7 +31,7 @@ class Test(APIView):
         data=UserSerializer(data,many=True)
         response.data=data.data
         response.code=200
-        response.msg="查询成功"
+        response.msg="ok"
         return JsonResponse(response.dict)
 
 
@@ -63,3 +68,120 @@ class CheckEmail(APIView):
             response.data = "null"
         finally:
             return JsonResponse(response.dict)
+
+
+class Reg(APIView):
+    def post(self,request):
+        response = BaseResponse()
+        username = request.data.get('username')
+        password = request.data.get('password')
+        email = request.data.get('email')
+        try:
+            obj = User.objects.create(username=username, password=md5(password), email=email)
+            response.msg = 'ok'
+            response.code = 200
+            response.data="null"
+        except Exception as e:
+            print(e)
+            response.msg = 'no'
+            response.code = 201
+            response.data = "null"
+
+        return JsonResponse(response.dict)
+
+
+class Login(APIView):
+     def post(self,request):
+         response = BaseResponse()
+         username = request.data.get('username')
+         password = request.data.get('password')
+         user = User.objects.filter(username=username, password=md5(password))
+         if user:
+             request.session['login'] = True
+             request.session['username']=username
+             response.msg = "ok"
+             response.data = "null"
+         else:
+                 response.msg = "no"
+                 response.code = 201
+                 response.data = "null"
+
+         return JsonResponse(response.dict)
+
+
+class IsLog(APIView):
+      def get(self,request):
+          response = BaseResponse()
+          try:
+            login=request.session['login']
+
+            if login:
+                username=request.session['username']
+                print(username)
+                user=User.objects.get(username=username)
+                img_size = user.img_set.count()
+                radio_size= user.radio_set.count()
+                doc_size= user.doc_set.count()
+                video_size = user.video_set.count()
+                data={
+                    'img':img_size,
+                    'radio':radio_size,
+                    'doc_size':doc_size,
+                    'video_size':video_size
+                }
+                response.code="200"
+                response.msg="ok"
+                response.data=data
+                return  JsonResponse(response.dict)
+            else:
+                response.code = "201"
+                response.msg = "no"
+                response.data = "null"
+                return JsonResponse(response.dict)
+          except Exception as e:
+
+              response.code = "201"
+              response.msg = "no"
+              response.data = "null"
+              return JsonResponse(response.dict)
+
+
+class OutLogin(APIView):
+        def get(self,request):
+            response = BaseResponse()
+            try:
+                del request.session['login']
+                del request.session['username']
+                response.code = "200"
+                response.msg = "yes"
+                response.data = "null"
+            except KeyError:
+                response.code = "201"
+                response.msg = "no"
+                response.data = "null"
+            return JsonResponse(response.dict)
+
+
+class CheckPassword(APIView):
+    def get(self,request):
+        response = BaseResponse()
+        try:
+            username = request.session['username']
+            password = request.query_params.dict()["password"]
+            user = User.objects.filter(username=username, password=md5(password))
+            if user:
+                response.code = "200"
+                response.msg = "ok"
+                response.data = "null"
+                return JsonResponse(response.dict)
+            else:
+                response.code = "201"
+                response.msg = "no"
+                response.data = "null"
+                return JsonResponse(response.dict)
+
+        except:
+              response.code = "201"
+              response.msg = "no"
+              response.data = "null"
+              return JsonResponse(response.dict)
