f18ab90196d0eec7df42215d4834da7bf69ea205
coder-fish <970710580@qq.com>
2020-05-27 12:21:12 +0800

    新加路由

--- wecloud/urls.py
+++ wecloud/urls.py
@@ -19,5 +19,6 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("api.urls"))
+    path('api/', include("api.urls")),
+    path('api/', include("sharefile.urls"))
 ]
