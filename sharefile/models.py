67b9a7c5d90a7877f0ba43176e93d9f10a9dbcc6
coder-fish <970710580@qq.com>
2020-05-27 12:20:11 +0800

    新建库

--- sharefile/models.py
+++ sharefile/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+from api import models
+from django.db import models
+
+
+class Share(models.Model):
+    share_id = models.IntegerField(primary_key=True, null=False)
+    file_id = models.IntegerField()
+    type = models.CharField(max_length=20, null=True)
+    path = models.CharField(max_length=255, null=True)
+    share_no = models.CharField(max_length=255, null=True)
+
+    class Meta:
+        db_table = "share"
