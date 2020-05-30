12af27fbeb17b9659d281baee13706eb3888fd0f
budebao <32029400+budebao@users.noreply.github.com>
2020-05-30 11:51:43 +0800

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,13 +4,13 @@ from django.urls import include
 from api import views
 urlpatterns = [
     path('test/',views.Test.as_view()),
-    path('users/checkUsername',views.CheckUsername.as_view()),
-    path('users/checkEmail',views.CheckEmail.as_view()),
-    path('users/reg',views.Reg.as_view()),
-    path('users/login',views.Login.as_view()),
-    path('users/isLog',views.IsLog.as_view()),
-    path('users/cheakPassword',views.CheckPassword.as_view()),
-    path('users/outLogin',views.OutLogin.as_view()),
+    path('checkUsername',views.CheckUsername.as_view()),
+    path('checkEmail',views.CheckEmail.as_view()),
+    path('reg',views.Reg.as_view()),
+    path('login',views.Login.as_view()),
+    path('isLog',views.IsLog.as_view()),
+    path('cheakPassword',views.CheckPassword.as_view()),
+    path('outLogin',views.OutLogin.as_view()),
     path('getFile',views.GetFile.as_view())
 
 ]
