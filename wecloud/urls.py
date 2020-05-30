33d77755ef0fc24d94c78f9a600aa051de101a42
yuchen125 <49783606+yuchen125@users.noreply.github.com>
2020-05-30 11:05:51 +0800

    Update urls.py

--- wecloud/urls.py
+++ wecloud/urls.py
@@ -20,5 +20,6 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("api.urls")),
-    path('sharefile/', include("sharefile.urls"))
+    path('sharefile/', include("sharefile.urls")),
+    path('file/', include("file.urls"))
 ]
